fe2fbf48fd9df79c2c3a7223803fd1f3735f01f9
sebast759 <41928138+sebast759@users.noreply.github.com>
2022-09-12 18:55:13 +0100

    seb_commit

---
+++ SM_updatesynch_github.bat
@@ -0,0 +1,10 @@
+cd  "%USERPROFILE%\Dropbox\Etudes\2021\_Website\sebast759.github.io"
+
+echo %git_command%
+git add --all
+git commit -m "seb_commit"
+git push
+
+
+ECHO waiting 5 seconds...
+CHOICE /C:AB /D A /T 8 > NUL
