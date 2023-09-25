78acbb2bc6c9b3d977b2133c0e778261c8f96b15
sebast759 <sebastien.maillard@yahoo.Fr>
2023-09-25 13:02:02 +0100

    seb_benq_commit

---
+++ adanson/adanson75/SM_updatesynch_github.bat
@@ -0,0 +1,11 @@
+cd  "%USERPROFILE%\Dropbox\Etudes\2021\_Website\sebast759.github.io\adanson\adanson75"
+
+echo %git_command%
+git add --all
+git commit -m "seb_benq_commit"
+git pull
+git push
+
+
+ECHO waiting 5 seconds...
+CHOICE /C:AB /D A /T 8 > NUL
